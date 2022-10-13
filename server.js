beea7e36eded4d6b009502a574c961252bea32cf
Andrew Dippel <a.t.dippel@gmail.com>
2022-10-13 21:43:14 +1100

    add function to read database file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { fstat } = require('fs');
 const path = require('path');
 const app = express();
 const db = require('./Develop/db/db.json')
@@ -14,6 +15,13 @@ app.get('/', (req, res) =>
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+});
+
+app.get('/api/notes', function (req, res) {
+    fs.readFile(__dirname + '/db/db.json', (err, req) => {
+        return res.json(json.parse(req)
+        
+    })
 });
 
 app.listen(PORT, () =>
