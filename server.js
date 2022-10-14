3a58f8b0ae2113ceda926b8c79d6e48c6e02fdc4
Andrew Dippel <a.t.dippel@gmail.com>
2022-10-14 19:29:02 +1100

    fix save and display new note

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const { json } = require('express');
 const express = require('express');
-const { fstat } = require('fs');
+const fs = require('fs');
 const path = require('path');
 const app = express();
-const db = require('./Develop/db/db.json')
+const db = require('./db/db.json')
 const PORT = 3001;
 
 app.use(express.json());
@@ -13,30 +12,36 @@ app.use(express.static('public'));
 
 //display index page
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 
 //display notes page
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 
 //get saved notes
 app.get('/api/notes', function (req, res) {
     fs.readFile(__dirname + '/db/db.json', (err, req) => {
-        return res.json(json.parse(req)
-        
+        return res.json(JSON.parse(req))
+
     })
 });
 
 //post new notes
 app.post('/api/notes', function (req, res) {
-    json.parse(fs.readFile(__dirname + '/db/db.json')).push(req.body);
-    json.parse(fs.readFile(__dirname + '/db/db.json')).forEach((note, i) => { note.id = i + 1 });
-    fs.writeFile(__dirname + '/db/db.json', json.stringify(json.parse(fs.readFile(__dirname + '/db/db.json'))));
-    res.json(json.parse(fs.readFile(__dirname + '/db/db.json')));
+    fs.readFile(__dirname + '/db/db.json', (err, data) => {
+        const notes = JSON.parse(data)
+        notes.push(req.body)
+        notes.forEach((note, i) => { note.id = i + 1 })
+        fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notes), (err, data) => {
+            res.json(notes);
+        });
+
+    });
+
 })
 
 app.listen(PORT, () =>
