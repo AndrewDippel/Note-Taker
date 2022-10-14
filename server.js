8e2ed17c5f1a9f939e7d8a9b71c9184911cd7e1e
Andrew Dippel <a.t.dippel@gmail.com>
2022-10-14 18:07:40 +1100

    add function to add new notes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const express = require('express');
 const { fstat } = require('fs');
 const path = require('path');
@@ -9,20 +10,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+
+//display index page
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
 );
 
+
+//display notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
 });
 
+
+//get saved notes
 app.get('/api/notes', function (req, res) {
     fs.readFile(__dirname + '/db/db.json', (err, req) => {
         return res.json(json.parse(req)
         
     })
 });
+
+//post new notes
+app.post('/api/notes', function (req, res) {
+    json.parse(fs.readFile(__dirname + '/db/db.json')).push(req.body);
+    json.parse(fs.readFile(__dirname + '/db/db.json')).forEach((note, i) => { note.id = i + 1 });
+    fs.writeFile(__dirname + '/db/db.json', json.stringify(json.parse(fs.readFile(__dirname + '/db/db.json'))));
+    res.json(json.parse(fs.readFile(__dirname + '/db/db.json')));
+})
 
 app.listen(PORT, () =>
     console.log(`App Listener port - http://localhost:${PORT}`)
